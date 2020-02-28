c7bd9eb5cdf43f52fb3f6fcdc758b79f09291e3f
prostov <knockjkeee@gmail.com>
2020-02-28 15:29:38 +0300

    Updates

---
+++ precache-manifest.9bdc960244df062d38f8dcf6771d1c72.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82bd25500597570e6ad09ede69c55bca",
+    "url": "/voda-app/index.html"
+  },
+  {
+    "revision": "37cc8c01690116185697",
+    "url": "/voda-app/static/css/2.a641e29a.chunk.css"
+  },
+  {
+    "revision": "e30b62f83fad88a8ff93",
+    "url": "/voda-app/static/css/main.4056d9f3.chunk.css"
+  },
+  {
+    "revision": "37cc8c01690116185697",
+    "url": "/voda-app/static/js/2.29c0a7fe.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/voda-app/static/js/2.29c0a7fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e30b62f83fad88a8ff93",
+    "url": "/voda-app/static/js/main.1208d4f6.chunk.js"
+  },
+  {
+    "revision": "88df7a90082a36644057",
+    "url": "/voda-app/static/js/runtime-main.dc54219c.js"
+  },
+  {
+    "revision": "88946e758057b5447d99fd6428d14b6a",
+    "url": "/voda-app/static/media/1.88946e75.jpeg"
+  },
+  {
+    "revision": "a90b0f86f471b7840c8c3b678620cbf3",
+    "url": "/voda-app/static/media/2.a90b0f86.jpg"
+  },
+  {
+    "revision": "f6d92e52803cb927a20dd12150cebcb3",
+    "url": "/voda-app/static/media/2.f6d92e52.jpeg"
+  },
+  {
+    "revision": "98977708e521dd79457bd64852c0bd56",
+    "url": "/voda-app/static/media/3.98977708.jpeg"
+  },
+  {
+    "revision": "20fc3b2902ddea9cf3ec0acc9b66b12f",
+    "url": "/voda-app/static/media/b1.20fc3b29.jpg"
+  },
+  {
+    "revision": "76176427f169b3aaee6c1d069792ec7b",
+    "url": "/voda-app/static/media/b2.76176427.jpg"
+  },
+  {
+    "revision": "62d1bdacaa205a747062ae110accd729",
+    "url": "/voda-app/static/media/b3.62d1bdac.jpg"
+  },
+  {
+    "revision": "ac1092a6177e575c60503875af5abe3b",
+    "url": "/voda-app/static/media/bg-masthead.ac1092a6.jpg"
+  }
+]);
